ae8a8a1271f56f923c1bb543c0bab06667004724
kblatsos <kblatsos@infili.com>
2022-08-02 21:58:37 +0300

    routing, layouts, dynamic params

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="single-recipe">
+    <h1>{{ recipe.title }}</h1>
+    <div>
+      <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title" />
+    </div>
+    <p>{{ recipe.descText }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipe: [
+            {
+              id: "1",
+              title: "Delicious pizza",
+              previewText: "Awesome pizza",
+              descText: "Some outstanding choices",
+              thumbnail:
+                "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60",
+            },
+            {
+              id: "2",
+              title: "Delicious burger",
+              previewText: "Perfect burger",
+              descText: "Some outstanding choices for you",
+              thumbnail: "https://media.oneman.gr/onm-images/burger-38.jpg",
+            },
+          ].find((el) => el.id === context.params.id),
+        });
+      }, 1500);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.single-recipe {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+.recipe-image {
+  width: 100%;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="recipes">
+    <Recipe
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :id="recipe.id"
+      :title="recipe.title"
+      :previewText="recipe.previewText"
+      :thumbnail="recipe.thumbnail"
+    />
+  </section>
+</template>
+
+<script>
+import Recipe from "../../components/Recipe.vue";
+export default {
+  components: {
+    Recipe,
+  },
+  asyncData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "1",
+              title: "Delicious pizza",
+              previewText: "Awesome pizza",
+              thumbnail:
+                "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60",
+            },
+            {
+              id: "2",
+              title: "Delicious burger",
+              previewText: "Perfect burger",
+              thumbnail: "https://media.oneman.gr/onm-images/burger-38.jpg",
+            },
+          ],
+        });
+      }, 1500);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+}
+.recipe {
+  box-sizing: border-box;
+  width: 280px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #aaa;
+}
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
+}
+</style>>
